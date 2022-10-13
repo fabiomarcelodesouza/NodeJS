87c2262a0cb04a535e8c36c27b6170d72a68bb19
Fabio Souza <fabio.souza@br.zurich.com>
2022-10-13 00:38:14 -0300

    versionando o modulo de impressao

---
+++ moduloDeImpressao/app.js
@@ -0,0 +1,4 @@
+const impressao = require('./moduloDeImpressao.js')
+
+const professores = ['Elias', 'Yuri', 'Gabriel', 'Guilherme', 'Yan'];
+new impressao().imprime(professores);
---
+++ moduloDeImpressao/moduloDeImpressao.js
@@ -0,0 +1,12 @@
+class ModuloDeImpressao {
+    constructor() {
+        this._codigo = 10;
+    }
+    imprime(nomes) {
+        nomes.forEach(function(nome) {
+        console.log(`${this._codigo}: ${nome}`);
+     }.bind(this));
+   }
+}
+
+module.exports = ModuloDeImpressao;
