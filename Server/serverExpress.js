7f572eca8ac9d930a6c3ed170e0af793d6d31d21
Fabio Souza <fabio.souza@br.zurich.com>
2022-10-13 00:36:25 -0300

    tentando mais uma vez vesionar essa poha

---
+++ Server/serverExpress.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    const valor = req.query.dadoExterno
+    const valorDoResponse = `Voce me mandou tal ${valor}`;
+    res.send(valorDoResponse)
+})
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`Servidor subiu com sucessinhos!
+                 http://localhost:${port}`)
+})
