d8d59c6bfb746a04dfc750588dc1d5f61813e3c2
Fabio Souza <fabmasouza@hotmail.com>
2022-11-04 19:01:59 -0300

    Criando metodos put e delete

--- orm-sequelize/api/controllers/PessoaController.js
+++ orm-sequelize/api/controllers/PessoaController.js
@@ -32,6 +32,31 @@ const database = require('../models')
             return res.status(500).json(error.message)
         }
     }
+
+    static async atualizarPessoa(req, res) {        
+        const novasInfos = req.body
+        const { id } = req.params
+
+        try {
+            await database.Pessoas.update(novasInfos, { where: { id: Number(id) } })
+            return res.status(200).json(await database.Pessoas.findOne( { where: { id: Number(id) } } ))
+        }
+        catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async deletarPessoa(req, res) {                
+        const { id } = req.params
+
+        try {
+            await database.Pessoas.destroy({ where: { id: Number(id) } })
+            return res.status(200).json({"mensagem":`Id ${id} Excluido com sucesso!`})
+        }
+        catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
 }
 
 module.exports = PessoaController
--- orm-sequelize/api/routes/pessoasRoute.js
+++ orm-sequelize/api/routes/pessoasRoute.js
@@ -6,5 +6,7 @@ const router = Router()
 router.get('/pessoas', PessoaController.consultarTodas)
 router.get('/pessoas/:id', PessoaController.consultarPessoa)
 router.post('/pessoas', PessoaController.criarPessoa)
+router.put('/pessoas/:id', PessoaController.atualizarPessoa)
+router.delete('/pessoas/:id', PessoaController.deletarPessoa)
 
 module.exports = router
